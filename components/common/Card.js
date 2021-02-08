791fd05169c64faba54b64e261572e9cefd25edf
“Akpughe” <davidakpughe2@gmail.com>
2021-02-08 19:53:06 +0100

    styling

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Card = ({ figure, job }) => {
+const Card = ({ figure, job, color }) => {
   return (
-    <div className="bg-white shadow-2xl border-" style={{ width: '200px' }}>
-      <div className="pt-12 ml-8">
+    <div
+      className={`bg-white shadow-2xl rounded-md ${color} `}
+      style={{ width: '300px', height: '100px' }}
+    >
+      <div className="pt-4 ml-8 text-white">
         <div>
           <h2 className="text-4xl">{figure}</h2>
         </div>
@@ -11,11 +14,11 @@ const Card = ({ figure, job }) => {
           <h5 className="text-sm font-normal">{job}</h5>
         </div>
       </div>
-      <div className="flex justify-end ">
+      {/* <div className="flex justify-end ">
         <div className="bg-purple-500 text-white opacity-90 w-12 h-12 rounded-tl-full flex justify-center items-center ">
           hi
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
